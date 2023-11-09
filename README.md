ac123463bc5b7a603a41f6b6494bec6519eff591
Gabriel Trigo <gabrielguerratrigo20@gmail.com>
2023-11-09 16:41:58 -0500

    Started part 3! Database is set up

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SciSearch
+
+ChatGPT Prompts
+
+1. How to use a .env file with my flask app to protect sensitive passwords?
+2. How use before_request and teardown_request to create the database connection and to close it.
+3. Show me how to add a database connection to flask
+4. Where should I store my template file when using flask to render Jinja2 template?
+5. Give me an example of a css style sheet
+6. Can I add more than one CSS stylesheet to my html header?
---
+++ part3/app.py
@@ -0,0 +1,80 @@
+import os
+from flask import Flask, g, render_template
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+app.static_folder = "static"
+load_dotenv()
+engine = create_engine(os.getenv("DATABASEURI"))
+
+
+'''
+    Method to connect to database.
+'''
+def get_db():
+    if 'db' not in g:
+        g.db = engine.connect()
+    return g.db
+
+
+'''
+    Method to close database connection.
+'''
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    cursor = db.execute(text("SELECT * FROM author"))
+    close_db()
+    context = {
+        "data": [ list(el) for el in cursor ]
+    }
+    return render_template("base.html", **context)
+
+
+@app.route("/author/<author_id>")
+def author_page(author_id):
+    db = get_db()
+    author_cursor = db.execute(
+        text(
+            f"""
+                SELECT * 
+                FROM author a
+                WHERE a.author_id = '{ author_id }'
+            """
+            ))
+    author = [list(el) for el in author_cursor]
+
+    if not author:
+        return "Author not found."
+    
+    paper_cursor = db.execute(
+        text(
+            f"""
+                SELECT p.paper_id, p.title,
+                    p.date_published, p.url, pb.publication_name
+                FROM paper p
+                LEFT JOIN publication pb
+                    ON p.publication_name = pb.publication_name
+                WHERE p.paper_id IN (
+                    SELECT paper_id
+                    FROM authored a
+                    WHERE a.author_id = '{ author_id }'
+                )
+            """
+        )
+    )
+    close_db()
+    papers = [list(el) for el in paper_cursor]
+    context = {
+        "author": author[0],
+        "papers": papers
+    }
+    return render_template("author.html", **context)
